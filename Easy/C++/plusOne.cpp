ad0bcfac6687db4bfca2e2dd797cc3e7f2f97206
Dylan Kuster <drkuster@crimson.ua.edu>
2022-04-18 22:41:32 -0500

    Better solution for 61

--- Easy/C++/plusOne.cpp
+++ Easy/C++/plusOne.cpp
@@ -1,31 +1,19 @@
 class Solution {
  public:
   vector<int> plusOne(vector<int>& digits) {
-    vector<int> result;
-    bool c = false;
-    int lD = digits.at(digits.size() - 1), i = digits.size() - 2;
-
-    if (lD == 9) {
-      result.push_back(0);
-    } else {  // job is done
-      digits.at(digits.size() - 1)++;
-      return digits;
-    }
+    int i = digits.size() - 1;
 
     while ((i >= 0) && (digits.at(i) == 9)) {
-      result.push_back(0);
+      digits.at(i) = 0;
       i--;
     }
 
-    if (i >= 0) {
-      result.push_back(digits.at(i) + 1);
-      for (int j = i - 1; j >= 0; j--) result.push_back(digits.at(j));
-    } else {
-      result.push_back(1);
+    if (i < 0) {
+      digits.insert(digits.begin(), 1);
+      return digits;
     }
 
-    reverse(result.begin(), result.end());
-
-    return result;
+    digits.at(i)++;
+    return digits;
   }
 };
