dc8c3354b76598ad0b0fed5971b2f8dfed9bd8ff
Dylan Kuster <drkuster@crimson.ua.edu>
2022-04-18 23:29:01 -0500

    o(lgn) solution to fixed point using binary search

--- Easy/C++/fixedPoint.cpp
+++ Easy/C++/fixedPoint.cpp
@@ -1,11 +1,26 @@
 class Solution {
  public:
   int fixedPoint(vector<int>& arr) {
-    for (int i = 0; i < arr.size(); i++) {
-      if (arr.at(i) == i) {
-        return i;
+    int l = 0, r = arr.size() - 1, min = -1, m;
+
+    while (l < r) {
+      m = (l + r) / 2;
+
+      if (arr.at(m) == m) {  // found at least one match but we might be able to
+                             // do better, move left
+        r = m - 1;
+        min = arr.at(m);
+      } else if (arr.at(m) < m) {  // too far left, move right
+        l = m + 1;
+      } else {  // only option is to move left
+        r = m - 1;
       }
     }
-    return -1;
+
+    if ((l == r) && (arr.at(l) == l)) {
+      min = arr.at(l);
+    }
+
+    return min;
   }
 };
